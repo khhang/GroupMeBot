61daa647f724ef3ba30e111aba9fd026efd00c58
Brian Pham <brian.qk.pham@gmail.com>
2016-08-27 18:02:43 +0000

    added emotes and roll dice function

--- groupMeHelper.php
+++ groupMeHelper.php
@@ -4,6 +4,7 @@
 	$GLOBALS['botURL'] = $configs['botURL'];
 	$GLOBALS['weatherKey'] = $configs['weatherKey'];
 	$GLOBALS['weatherURL'] = $configs['weatherURL'];
+	$GLOBALS['token'] = $configs['token'];
 	$GLOBALS['log'] = $configs['log'];
 
 	function LogMessage($file,$message){
@@ -12,12 +13,16 @@
 		fclose($fp);
 	}
 
+	//
+	//Start of bot commands
+	//
+
 	// Run bot command
 	function RunCommand($text){
 		 LogMessage($GLOBALS['log'], "Inside RunCommand");
-		if(preg_match("/(![A-Za-z]+)\s(([A-Za-z0-9](\s)?)+)/",$text,$matches)){
+		if(preg_match("/(!([A-Za-z]+))(\s(([A-Za-z0-9](\s)?)+))?/",$text,$matches)){
 			$command = $matches[1];
-			$details = $matches[2];
+			$details = $matches[3];
 			 LogMessage($GLOBALS['log'], "Found a match.");
 		}
 
@@ -26,11 +31,14 @@
 			BotEcho($details);
 		}elseif($command == "!weather"){
 			BotWeather($details);
+		}elseif($command == "!roll"){
+			BotRoll($details);
 		}elseif($command != NULL){
-			BotEcho("Invalid command.");
+			BotEmoteCheck($matches[2]);
 		}
 	}
 
+	// Gets current weather for specified location
 	function BotWeather($loc){
 		$url = $GLOBALS['weatherURL'];
 		$params = array("key" => $GLOBALS['weatherKey'], "q" => $loc);
@@ -48,7 +56,7 @@
 	            	"\nCurrent Temp(F): " . $jsonresult->current->temp_f . ", " . $jsonresult->current->condition->text .
 	            	"\nFeels Like(F): " . $jsonresult->current->feelslike_f;
 			}else{
-				$weatherInfo = "An error has occured. Please check inputs and try again.";
+				$weatherInfo = "Error my bomb is no working hehe my goat xd";
 			}
 			BotEcho($weatherInfo);
 		}
@@ -72,9 +80,73 @@
 	     if($result === FALSE){ LogMessage($GLOBALS['log'], "No results."); }
 	}
 
+	function BotRoll($max){
+		if($max != NULL){
+			BotEcho("Rolled " . strval(rand(1,$max)) . " out of $max");
+		}else{
+			BotEcho("Rolled " . strval(rand(1,6)) . " out of 6");
+		}
+	}
+
+	// Checks if emote exists
+    function BotEmoteCheck($emote){
+        LogMessage($GLOBALS['log'], "Testing upload");
+		$imageURL = "https://image.groupme.com/pictures?access_token=".$GLOBALS['token'];
+		$filename = realpath("./Emotes/".$emote.".png");
+		$image = array("file" => "@".$filename)
+
+		$ch = curl_init($imageURL);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $image);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+		curl_close($ch);
+
+        if($result === FALSE){
+			LogMessage($GLOBALS['log'], "No results.");
+			BotEcho("Invalid command");
+		}else{
+			LogMessage($GLOBALS['log'], $result);
+			$jsondata = json_decode($result);
+			BotPostEmote($jsondata->payload->picture_url);
+		}
+    }
+
+
+	// Post Emote to group chat
+	function BotPostEmote($imageURL){
+		LogMessage($GLOBALS['log'], "Unleashing the KAPPA.");
+		$url = $GLOBALS['botURL'];
+		$reply = array(
+			"bot_id" => $GLOBALS['botID'], 
+			"attachments" => array(array(
+				"type" => "image",
+				"url" => $imageURL
+				))
+			);
+		$options = array(
+			"http" => array(
+				"header" => "Content-type: application/json\r\n", 
+				"method" => "POST", 
+				"content" => json_encode($reply)
+				)
+			);
+
+		$context = stream_context_create($options);
+		LogMessage($GLOBALS['log'], json_encode($reply));
+		$result = file_get_contents($url, false, $context);
+
+		if($result === FALSE){ LogMessage($GLOBALS['log'], "No results."); }
+
+	}
+
+	//
+	// End of bot commands
+	//
+
 	// Grab last message from GroupMe POST request
 	function GrabText($json){
-
+		//LogMessage($GLOBALS['log'], $json);
 		// Iterate through data
 	    $jsonIterator = new RecursiveIteratorIterator(
 	        new RecursiveArrayIterator(json_decode($json, TRUE)),
@@ -87,5 +159,5 @@
 			}
 		}
 	}
-
+	
 ?>
